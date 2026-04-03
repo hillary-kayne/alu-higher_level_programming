117e43e8528e228720edda7c7bd8fbf456f86a29
hillary-kayne <h.kayinabab@alustudent.com>
2026-04-03 10:10:33 -0700

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -20,4 +20,26 @@ elf, value):
                                                                                                                                                     elif value < 0:
                                                                                                                                                                     raise ValueError("height must be >= 0")
                                                                                                                                                                         else:
+                                                                                                                                                                                        self.__height = valuelf, value):
+            """Set the rectangle's width."""
+                    if type(value) is not int:
+                                    raise TypeError("width must be an integer")
+                                        elif value < 0:
+                                                        raise ValueError("width must be >= 0")
+                                                            else:
+                                                                            self.__width = value
+
+                                                                                @property
+                                                                                    def height(self):
+                                                                                                """Get the rectangle's height."""
+                                                                                                        return self.__height
+
+                                                                                                        @height.setter
+                                                                                                            def height(self, value):
+                                                                                                                        """Set the rectangle's height."""
+                                                                                                                                if type(value) is not int:
+                                                                                                                                                raise TypeError("height must be an integer")
+                                                                                                                                                    elif value < 0:
+                                                                                                                                                                    raise ValueError("height must be >= 0")
+                                                                                                                                                                        else:
                                                                                                                                                                                         self.__height = value
