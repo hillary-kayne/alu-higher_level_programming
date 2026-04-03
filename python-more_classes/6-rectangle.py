93422e2a150553036c7bf3062214a1b44faca88c
hillary-kayne <h.kayinabab@alustudent.com>
2026-04-03 10:36:13 -0700

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,14 +4,19 @@
 
 class Rectangle:
     '''empty class'''
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''initializing the instances in class'''
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         '''prints string to be deleted'''
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
